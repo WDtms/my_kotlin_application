373cc6ee9955d4b53bfd9961b089f6b8289be009
Шепелев Алексей <alekseyshepelev@inbox.ru>
2021-05-09 17:34:44 +1000

    Добавлены ресурсы картинок

--- app/src/main/java/com/example/my_kotlin_application/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/my_kotlin_application/fragments/update/UpdateFragment.kt
@@ -1,10 +1,11 @@
-package com.example.my_kotlin_application
+package com.example.my_kotlin_application.fragments.update
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.my_kotlin_application.R
 
 class UpdateFragment : Fragment() {
 
